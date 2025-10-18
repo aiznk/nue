1f64a1e8e57ff2db7a6339239da19533e471b038
noname <nothing@nothing>
2025-10-18 14:16:00 +0900

    ok

--- src/main.js
+++ src/main.js
@@ -46,15 +46,275 @@ export function ref (value) {
 	return proxy
 }
 
+class TemplateTag {
+	constructor () {
+		this.name = null
+		this.attrs = {}
+		this.type = 'begin'
+		this.text = ''
+		this.index = 0
+		this.code = null
+	}
+
+	skipSpaces () {
+		for (; this.index < this.code.length; this.index++) {
+			let c = this.code[this.index]
+			if (c === ' ' || c === '　') {
+				// pass
+			} else {
+				break
+			}
+		}
+	}
+
+	parseName () {
+		this.skipSpaces()
+		this.name = ''
+
+		if (this.code[this.index] === '/') {
+			this.type = 'end'
+			this.index++
+		}
+
+		for (; this.index < this.code.length; this.index++) {
+			let c = this.code[this.index]
+			// console.log(`name: [${c}]`)
+			if (c === '>' || c === ' ') {
+				break
+			} else if (c === '/') {
+				this.type = 'close'
+			} else {
+				this.name += c
+			}
+		}
+
+		this.skipSpaces()
+	}
+
+	parseAttrKey () {
+		this.skipSpaces()
+		let key = ''
+		for (; this.index < this.code.length; this.index++) {
+			let c = this.code[this.index]
+			// console.log(`key: [${c}]`)
+			if (c === '/') {
+				this.type = 'end'
+			} else if (c === '>' || c === '=' ||
+			           c === ' ' || c === '　') {
+				break
+			} else {
+				key += c
+			}
+		}		
+		this.skipSpaces()
+		return key
+	}
+
+	parseAttrValue () {
+		this.skipSpaces()
+		let val = ''
+		let m = 0
+		if (this.code[this.index] === '"') {
+			m = 10
+			this.index++
+		}
+		for (; this.index < this.code.length; this.index++) {
+			let c = this.code[this.index]
+			// console.log(`value: [${c}]`)
+			if (m === 0) {
+				if (c === '/') {
+					this.type = 'close'
+				} else if (c === '"' || 
+					c === ' ' || c === '　' || c === '>') {
+					break
+				} else {
+					val += c
+				}
+			} else if (m === 10) {
+				if (c === '"') {
+					break
+				} else {
+					val += c
+				}
+			}
+		}
+		this.skipSpaces()
+		return val
+	}
+
+	parse (i, code) {
+		this.index = i
+		this.code = code
+		let m = 0
+
+		// console.log(`parse enter: [${this.code[this.index]}]`)
+		if (this.code[this.index] === '<') {
+			m = 0 // tag
+			this.index++
+		} else {
+			m = 10 // text content
+		}
+
+		if (m === 0) {
+			this.parseName()
+			// console.log('parseName', this.name)
+
+			for (; this.index < this.code.length; ) {
+				this.skipSpaces()
+				let c = this.code[this.index]
+				// console.log(`parse: [${c}]`)
+
+				if (c === '>') {
+					this.index++
+					break
+				} else if (c === '/') {
+					this.type = 'close'
+					this.index++
+					continue
+				}
+
+				let key = this.parseAttrKey()
+				this.skipSpaces()
+				c = this.code[this.index]
+				if (c === '>') {
+					this.attrs[key] = null
+					break
+				} else if (c === '=') {
+					this.index++
+					let val = this.parseAttrValue()
+					this.attrs[key] = val
+				} else if (c === ' ' || c === '　') {
+					this.attrs[key] = null
+				}
+			}
+		} else {
+			this.type = 'text'
+
+			for (; this.index < this.code.length; this.index++) {
+				let c = this.code[this.index]
+				if (c === '<') {
+					break
+				} else {
+					this.text += c
+				}
+			}
+		}
+
+		return this.index
+	}
+}
+
+class Template {
+	constructor () {
+		this.tTags = []
+		this.root = new Tag('TemplateRoot')
+		this.components = {}
+	}
+
+	parse (code, {
+		components={},
+	}={}) {
+		this.tTags = []
+		this.components = components
+
+		for (let i = 0; i < code.length; ) {
+			let tag = new TemplateTag()
+			i = tag.parse(i, code)
+			this.tTags.push(tag)
+		}
+
+		// console.log(Object.assign([], this.tTags))
+		this.tree({
+			parent: this.root,
+			children: this.root.children,
+		})
+	}
+
+	hasComponent (name) {
+		return name in this.components
+	}
+
+	getComponent (name) {
+		return this.components[name]
+	}
+
+	tree ({
+		begin=null,
+		parent=null,
+		children=[],
+		dep=0,
+	}={}) {
+		if (!this.tTags.length) {
+			return
+		}
+
+		let tag = this.tTags.shift()
+		// console.log('tree:', dep, tag.name, tag.type)
+
+		/*
+			<div>
+				<span>123</span>
+				<p>
+					<span>223</span>
+				</p>
+			</div>
+		*/
+		if (tag.type === 'begin') {
+			let compo
+			if (this.hasComponent(tag.name)) {
+				compo = this.getComponent(tag.name)
+			} else {
+				compo = new Tag(tag.name, tag.attrs)
+			}
+			compo.parent = parent
+			parent = compo
+			this.tree({ 
+				begin: tag,
+				parent,
+				children: compo.children,
+				dep: dep+1,
+			})
+			children.push(compo)
+		} else if (tag.type === 'end') {
+			if (begin) {
+				if (begin.name.toLowerCase() === tag.name.toLowerCase()) {
+					return
+				}
+			}
+		} else if (tag.type === 'close') {
+			let compo
+			if (this.hasComponent(tag.name)) {
+				compo = this.getComponent(tag.name)
+			} else {
+				compo = new Tag(tag.name, tag.attrs)
+			}
+			compo.parent = parent
+			parent = compo
+			children.push(compo)			
+		} else if (tag.type === 'text') {
+			let compo = new Tag('text')
+			compo.text = tag.text
+			children.push(compo)
+		}
+
+		this.tree({ begin, parent, children, dep:dep+1 })
+	}
+}
+
 export class Component {
 	constructor (name, attrs={}) {
 		this.name = name
-		this.attrs = attrs
+		this.attrs = Object.assign({}, attrs)
 		this.parent = attrs.parent || null
 		this.children = []
 		this.elem = document.createElement(name)
 		this.setElementAttrs(attrs)
 		this.bindEvents()
+	}
+
+	template (code) {
+		let tmpl = new Template()
+		tmpl.parse(code)
 	}
 
 	gen (cname, ...args) {
@@ -406,4 +666,62 @@ export class Root extends Component {
 		this.mountElem.innerHTML = ''
 		this.mountElem.appendChild(this.elem)
 	}
+}
+
+export function test () {
+	let t
+
+	t = new Template()
+	t.parse('<div><span>123</span><p id="223" maxlength=323>423</p><img alt="hige" /></div>')
+	// console.log(t)
+	console.assert(t.root.children.length === 1)
+	console.assert(t.root.children[0].name === 'div')
+	console.assert(t.root.children[0].children.length === 3)
+	console.assert(t.root.children[0].children[0].name === 'span')
+	console.assert(t.root.children[0].children[0].children.length === 1)
+	console.assert(t.root.children[0].children[0].children[0].text === '123')
+	console.assert(t.root.children[0].children[1].name === 'p')
+	console.assert(t.root.children[0].children[1].attrs['id'] === '223')
+	console.assert(t.root.children[0].children[1].attrs['maxlength'] === '323')
+	console.assert(t.root.children[0].children[1].children.length === 1)
+	console.assert(t.root.children[0].children[1].children[0].text === '423')
+	console.assert(t.root.children[0].children[2].name === 'img')
+	console.assert(t.root.children[0].children[2].attrs['alt'] === 'hige')
+
+	t = new Template()
+	let Hoge = new Tag('Hoge')
+	let Moge = new Tag('Moge')
+	let Oge = new Tag('Oge')
+	t.parse(`<div><Hoge>123</Hoge><Moge id="223" maxlength=323>423</Moge><Oge age=20 weight="60" /></div>`, {
+		components: {
+			Hoge, Moge, Oge,
+		},
+	})
+	// console.log(t)
+	console.assert(t.root.children.length === 1)
+	console.assert(t.root.children[0].name === 'div')
+	console.assert(t.root.children[0].children.length === 3)
+	console.assert(t.root.children[0].children[0].name === 'Hoge')
+	console.assert(t.root.children[0].children[1].name === 'Moge')
+	console.assert(t.root.children[0].children[2].name === 'Oge')
+
+	t = new Template()
+	t.parse(`aaa<div>bbb<p>ccc</p>ddd</div>eee`)
+	console.assert(t.root.children.length === 3)
+	console.assert(t.root.children[0].name === 'text')
+	console.assert(t.root.children[0].text === 'aaa')
+	console.assert(t.root.children[1].name === 'div')
+	console.assert(t.root.children[1].children.length === 3)
+	console.assert(t.root.children[1].children[0].name === 'text')
+	console.assert(t.root.children[1].children[0].text === 'bbb')
+	console.assert(t.root.children[1].children[1].name === 'p')
+	console.assert(t.root.children[1].children[1].children.length === 1)
+	console.assert(t.root.children[1].children[1].children[0].name === 'text')
+	console.assert(t.root.children[1].children[1].children[0].text === 'ccc')
+	console.assert(t.root.children[1].children[2].name === 'text')
+	console.assert(t.root.children[1].children[2].text === 'ddd')
+	console.assert(t.root.children[2].name === 'text')
+	console.assert(t.root.children[2].text === 'eee')
+
+	console.log('OK')
 }
