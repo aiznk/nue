a653a437e030856437e9b0846c8b080d3063e31b
aiznk <nothing@nothing>
2026-05-24 12:54:20 +0900

    build

--- nue.js
+++ nue.js
@@ -453,6 +453,8 @@ export class Component {
 			'pointercancel': this.onPointerCancel.bind(this),
 			'wheel': this.onWheel.bind(this),
 			'scroll': this.onScroll.bind(this),
+			'focus': this.onFocus.bind(this),
+			'blur': this.onBlur.bind(this),
 		}
 		const events = _oget(this.opts, 'events', null)
 		if (events == null) {
@@ -488,6 +490,8 @@ export class Component {
 	async onMouseEnter (ev) {}
 	async onMouseLeave (ev) {}
 	async onContextMenu (ev) {}
+	async onFocus (ev) {}
+	async onBlur (ev) {}
 
 	async emit (name, value) {
 		if (this.parent) {
@@ -677,6 +681,10 @@ export class Component {
 
 	focus () {
 		this.elem.focus()
+	}
+
+	blur () {
+		this.elem.blur()
 	}
 }
 
